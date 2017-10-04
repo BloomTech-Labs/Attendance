f2f82e760d4986fd22a0bc85405db70bb23b523f
Emily Sperry <sperrye@gmail.com>
2017-10-04 15:26:53 -0700

    node server

--- request.js
+++ request.js
@@ -1,37 +1,58 @@
-const request = require('request');
+const express = require('express');
 const moment = require('moment');
+const zoom = require('zoomus')({
+  'key': api_key,
+  'secret': api_secret
+});
 
-const headers = {
-    'accept': 'application/json',
-    'content-type': 'application/x-www-form-urlencoded'
-};
+const app = express();
 
-const startDate = moment().format().split('-').join('%2F').slice(0,14);
-const endDate = startDate;
-
-const dateRange = `from=${startDate}&to=${endDate}`
-const api_key = '';
-const api_secret = '';
-const type = 'type=2'; //Live Meetings: 1, Past Meetings: 2
-const defaults = 'page_size=30&page_number=1';
-
-
-const dataString = `${api_key}&${api_secret}&${type}&${dateRange}&${defaults}`;
-
-
-const options = {
-    url: 'https://api.zoom.us/v1/metrics/meetings',
-    method: 'POST',
-    headers: headers,
-    body: dataString
-};
-
-const getMeetings = (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-        const meetings = JSON.parse(body)['meetings'];
-        meetings.forEach((meeting) => console.log(meeting.id));
-    }
+//const startDate = moment().format().split('-').join('%2F').slice(0,14);
+//console.log(startDate);
+let dashboard = {
+  type: 2,
+  from: '2017-10-04',
+  to: '2017-10-04',
 }
 
+const getUsers = (group) => {
+  console.log('Listing meeting participants...');
+  group[0]['participants'].forEach((participant) => {
+    if (participant) console.log(participant.user_name);
+  })
+  console.log('Success');
+}
 
-request(options, getMeetings);
+const getDetail = (ids) => {
+  console.log('Getting meeting participants...');
+  ids.forEach((id) => {
+    dashboard = {
+      meeting_id: id,
+      type: 2
+    }
+    zoom.dashboard.meeting(dashboard, (res) => {
+      if (res.error) console.log('error on getDetail');
+      const group = res;
+      getUsers([group]);
+    })
+  })
+}
+
+const getMeetings = () => {
+  console.log('getting meeting details...');
+  zoom.dashboard.meetings(dashboard, (res) => {
+    if (res.error) console.log('error');
+    let ids = [];
+    res.meetings.forEach((meeting) => ids.push(meeting.uuid));
+    console.log('Getting details for '+ids.length+' meetings...');
+    getDetail(ids);
+  })
+}
+
+app.get('/', (req, res) => {
+  console.log('getting meetings...');
+  let cohorts = [];
+  getMeetings();
+})
+
+app.listen(3000);
