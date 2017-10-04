38f6118e30a58b7f64f5abd8b6261a7307d0bfc7
Emily Sperry <sperrye@gmail.com>
2017-10-04 16:02:08 -0700

    type 2 requires past dates

--- request.js
+++ request.js
@@ -7,23 +7,30 @@ const zoom = require('zoomus')({
 
 const app = express();
 
-//const startDate = moment().format().split('-').join('%2F').slice(0,14);
-//console.log(startDate);
+// const startDate = moment().format().split('-').join('%2F').slice(0,14);
+// console.log(startDate);
 let dashboard = {
   type: 2,
-  from: '2017-10-04',
-  to: '2017-10-04',
+  from: '2017-10-03',
+  to: '2017-10-03',
 }
 
-const getUsers = (group) => {
-  console.log('Listing meeting participants...');
-  group[0]['participants'].forEach((participant) => {
-    if (participant) console.log(participant.user_name);
-  })
-  console.log('Success');
+const testCB = (username) => {
+  return username+'*';
 }
+
+// const getUsers = (group) => {
+//   console.log('Listing meeting participants...');
+//   group[0]['participants'].forEach((participant) => {
+//     if (participant) {
+//       console.log(testCB(participant.user_name));
+//     }
+//   })
+//   console.log('Success');
+// }
 
 const getDetail = (ids) => {
+  let classTitle = [];
   console.log('Getting meeting participants...');
   ids.forEach((id) => {
     dashboard = {
@@ -32,9 +39,14 @@ const getDetail = (ids) => {
     }
     zoom.dashboard.meeting(dashboard, (res) => {
       if (res.error) console.log('error on getDetail');
-      const group = res;
-      getUsers([group]);
+      const group = [res];
+      group.forEach((m) => {
+        classTitle.push(m.participants);
+      });
+      //res= {participants}{participants}{participants}{participants:[{users}]}
+      //getUsers([group]);
     })
+    console.log(classTitle);
   })
 }
 
@@ -51,7 +63,6 @@ const getMeetings = () => {
 
 app.get('/', (req, res) => {
   console.log('getting meetings...');
-  let cohorts = [];
   getMeetings();
 })
 
